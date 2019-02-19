70ff79e6489dae16729126b05db36154bc98927f
declanmcgee01 <declanmcgee1995@gmail.com>
2019-02-19 10:04:14 +0000

    Init

---
+++ src/Components/MainScreen.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class MainScreen extends Component {
+  render() {
+    return (
+      <div className="App">
+        Hello From MainScreen
+      </div>
+    );
+  }
+}
+
+export default MainScreen;
---
+++ src/Components/UpdateForm.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class UpdateForm extends Component {
+  render() {
+    return (
+      <div className="App">
+        Hello From UpdateForm
+      </div>
+    );
+  }
+}
+
+export default UpdateForm;
