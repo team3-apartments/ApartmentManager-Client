122712f11116b9c698986de51abb5dbd5412cd44
henrychia12 <declanmcgee1995@gmail.com>
2019-02-19 18:27:25 +0000

    overview table with colours

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import DeleteApartment from './Components/DeleteApartment';
 import AddApartment from './Components/AddApartment';
 import Login from './Components/Login';
 import FilterBar from './Components/FilterBar';
+import OverviewTable from './Components/OverviewTable';
 
 
 
@@ -23,6 +24,7 @@ class App extends Component {
           <div className= "Mainpara">
           
            <Route exact path="/" component={Login} />
+           <Route exact path="/overviewtable" component={OverviewTable} /> 
            {/* {(sessionStorage.getItem("Account"))=== null? <Route exact path="/addapartment" component={Login} />:<Route exact path="/addapartment" component={AddApartment} />}\*/} <Route exact path="/addapartment" component={AddApartment} /> 
            {/* {(sessionStorage.getItem("Account"))=== null? <Route exact path="/delete" component={Login} />:<Route exact path="/delete" component={DeleteForm} /> } */} <Route exact path="/deleteapartment" component={DeleteApartment} />
            {/* {(sessionStorage.getItem("Account"))=== null? <Route exact path="/updatetrainee" component={Login} />:<Route exact path="/updatetrainee" component={UpdateTrainee} />  } */} <Route exact path="/updateform" component={UpdateForm} />
